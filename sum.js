e268c34f5ada27b432f96e10d1c6df1920435686
Liina Adamson <liina.adamson@tptlive.ee>
2020-03-06 13:15:03 +0200

    Update sum.js

--- sum.js
+++ sum.js
@@ -1,9 +1,9 @@
 function randonthindgmidagi(a = 0, b = 'kala') {
   const a1 = Number(a);
   const b1 = Number(b);
-  if (Number.isNaN(a1) || Number.isNaN(b1)) {
+  if (Number.isNaN(a11) || Number.isNaN(b11)) {
     throw new Error('Invalid arguments');
   }
-  return a1 + b1 + c1;
+  return a1 + b1;
 }
 module.exports = randonthindgmidagi;
